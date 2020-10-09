5724db65cfc8555a38c5e2d0616a04541c748843
TestName <delf630@gmail.com>
2020-10-09 13:59:03 +0300

    Added some random or chosen colors

--- scripts.js
+++ scripts.js
@@ -5,16 +5,17 @@ $(".new-task__btn").on('click', function(e){
     let taskTxt = $('.new-task__input').val();
 
     if (taskTxt.trim().length === 0){
-        alert("Put something not empty!")
+        alert("Put something")
         return
     }
 
-    let task = ('<li>' + '<div class = "to-do-list__item task">'
+    let colors = ["red", "pink", "mediumpurple", "navy", "deepskyblue", "yellow"];
+    let style = colors[Math.round(Math.random() * 5)];
+    let task = ('<li>' + '<div class = "to-do-list__item' + ' ' + style + ' ' +'task">'
         + '<div class = "task__manage" >' + '<input class = "task__checkbox" type = "checkbox">'
         + '</div>' + '<div class = "task__body">'+ taskTxt +'</div>' + '</div>' + '</li>');
     //tasks.push(taskTxt);
     $("#to-do-list").append(task);
-    $(".to-do-list__item").css('background-color', "red");
 
 });
 
@@ -28,8 +29,7 @@ $(".colors__item").on('click', function (e){
     let bgColor = $(e.target).css('background-color');
 
     for (let i = 0; i < checked.length; i++){
-        //$(checked[i]).parent().parent().parent().parent().css('background', bgColor)
-        $(checked[i]).closest(".to-do-list__item").css('background', bgColor)
+        $(checked[i]).closest(".to-do-list__item").css('background-color', bgColor)
     }
 });
 
