1a75ae6fdcc5d6f70d5913783c022e09c418ea12
TestName <delf630@gmail.com>
2020-10-09 09:37:07 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,3 @@
+let task = '<li>' + '<div class = "to-do-list__item task">' + '<div class = "task__manage" >' + '<input class = "task__checkbox" type = "checkbox" checked >' +
+    '</div>' + '<div class = "task__body"> Сделать то, сделать се </div>' + '</div>' + '</li>'
+$("body").append(task);
